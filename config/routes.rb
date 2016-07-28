852d40e15be6c020d566ef553763e5efe7b75d77
EvanBrightside <xvanx84@gmail.com>
2016-07-28 18:28:41 +0300

    add controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :cards, only: [:new, :create]
+  root to: "cards#new"
+
+  get 'cards/new'
+
+  get 'cards/create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
